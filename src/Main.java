4e72dfd8ffbfbcdda1667bd79ff554d3a840f7f1
kpaul <kpaulina1995@gmail.com>
2021-09-26 09:17:45 +0200

    Zadanie 2.

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,51 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
 
-        int zmiennaInt = 7;
-        System.out.println("zmienna int = " + zmiennaInt);
-        /* ćw.2 */
+        int zmInt = 7;
+        System.out.println("zmienna int = " + zmInt);
+        byte zmByte = 55;
+        System.out.println("zmienna byte = " + zmByte);
+        short zmShort = 124;
+        System.out.println("zmienna short = " + zmShort);
+        long zmLong = 5679385;
+        System.out.println("zmienna long = " + zmLong);
+        float zmFloat = 23.12f;
+        System.out.println("zmienna float = " + zmFloat);
+        double zmDouble = 23456.76;
+        System.out.println("zmienna double = " + zmDouble);
+        boolean zmBoolean = false;
+        System.out.println("zmienna int = " + zmBoolean);
+        char zmChar = 'c';
+        System.out.println("zmienna int = " + zmChar);
 
-        /*   zad.1 zadeklaruj i zainicjalizuj zmienne różnych typów i wyświetl w konsoli (im więcej tym lepiej ;) ) */
+        int a = 12;
+        int b = 6;
+        int dodawanie = a+b;
+        System.out.println(dodawanie);
+        int odejmowanie = a-b;
+        System.out.println(odejmowanie);
+        int mnozenie = a*b;
+        System.out.println(mnozenie);
+        double dzielenie = a/b;
+        System.out.println(dzielenie);
+        int modulo = a%b;
+        System.out.println(modulo);
 
-        /*
-        *    zad.2 Napisz kod który będzie wykonywał operacje: dodawania, odejmowania,
-        *    mnożenia, dziealenia i modulo na zmiennych:
-        *    a) liczbaA i liczbaB, dowolne liczby calkowite (int),
-        *    b) liczbaX i liczbaY, dowolne liczby zmiennoprzecinkowe (double)
-        *  */
+        double x = 45.7;
+        double y = 34.6;
+        double dodawanieD = x+y;
+        System.out.println(dodawanieD);
+        double odejmowanieD = x-y;
+        System.out.println(odejmowanieD);
+        double mnozenieD = x*y;
+        System.out.println(mnozenieD);
+        double dzielenieD = x/y;
+        System.out.println(dzielenieD);
+        double moduloD = x%y;
+        System.out.println(moduloD);
 
 
 
