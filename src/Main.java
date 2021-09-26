1e16d2dbaf9a06c2ddaccd18513c41f91d944fb7
kpaul <kpaulina1995@gmail.com>
2021-09-26 10:47:19 +0200

    Zadanie 5.

--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,24 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        int wiek = 26;
 
-        /* ćw.5 */
+        if (wiek % 3 == 0) {
+            System.out.println("Wiek jest podzielny przez 3");
+        } else {
+            System.out.println("Wiek jest niepodzielny przez 3");
+        }
+        int nr_indeksu = 59387;
+        int wynik = (nr_indeksu % 2 == 0) ? 0 : 1;
 
-        /* zad.1 podziel swój wiek przez modulo 3 i jeśli twój wiek zwraca 0 wyświetl napis "Podzielny przez 3",
-         * w innym wypadku "Niepodzielny przez 3" użyj kontrukcji if else
-         * */
-
-
-        /* zad.2 użyj parametru trójargumentowego aby sprawdzić parzystośc nr. indeksu */
-
-
-        /* zad.3 wpisz z klawiatury liczbę zmiennoprzecinkową (użyj klasy Scanner z poprzednich ćwiczeń)
-         * i stwórz konstrukcję else if z dowolnymi komunikatami */
+        if (wynik == 1)
+        {
+            System.out.println("Nr indeksu jest nieparzysty");
+        }
+        else
+        {
+            System.out.println("Numer indeksu jest parzysty");
+        }
 
     }
 }
