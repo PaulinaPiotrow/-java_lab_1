a77386a42e6382f84020bd7151ff4ae29796d065
kpaul <kpaulina1995@gmail.com>
2021-09-26 08:36:12 +0200

    Zadanie 1

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
 public class Main {
 
-    public static void main(String[] args) {
-
-        System.out.println("Witaj świecie!");
-
+    public static void main(String[] args)
+    {
+        System.out.println("Napiszę cokolwiek");
+        System.out.println("Cieszę" + " " + "się" + " " + "że" + " weekend spędzam na uczelni");
+        System.out.printf("To są %s %d zajęcia. \n", "moje", 2);
+        System.out.print("Lubię moje ");
+        System.out.print("dwa piękne ");
+        System.out.print("psiaki.");
     }
 }
