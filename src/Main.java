7cab99a5003cf94d914f53a323dc03f4dfaa1c4e
kpaul <kpaulina1995@gmail.com>
2021-09-26 10:59:17 +0200

    Zadanie 5.

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        int wiek = 26;
+        int wiek = 25;
 
         if (wiek % 3 == 0) {
             System.out.println("Wiek jest podzielny przez 3");
@@ -13,14 +13,29 @@ public class Main {
         int nr_indeksu = 59387;
         int wynik = (nr_indeksu % 2 == 0) ? 0 : 1;
 
-        if (wynik == 1)
-        {
+        if (wynik == 1) {
             System.out.println("Nr indeksu jest nieparzysty");
-        }
-        else
-        {
+        } else {
             System.out.println("Numer indeksu jest parzysty");
         }
 
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Podaj liczbę: ");
+        int liczba = scan.nextInt();
+
+        if (liczba < 21)
+        {
+            System.out.println("Liczba jest mniejsza niż 21");
+        }
+
+        else if (liczba == 25)
+        {
+            System.out.println("Liczba jest równa 25");
+        }
+        else
+        {
+            System.out.println("Liczba nie jest mniejsza niż 21 i nie jest równa 25");
+        }
     }
 }
