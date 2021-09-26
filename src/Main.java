1d48101f7a09a389991a95a0f3a7e137be0f439c
kpaul <kpaulina1995@gmail.com>
2021-09-26 10:11:35 +0200

    Zadanie 3.

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,18 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+        boolean a = true;
+        boolean b = false;
+        boolean c = false;
 
-        /* ćw.4 */
+        System.out.println("(a||b) && (a&&!c) : " + ((a||b) || (a && !c)));
+        System.out.println("(!b&&c) || (a||!c) : " + ((!b&&c) || (a||!c)));
+        System.out.println("(a&&c) && (b||c) : " + ((a&&c) && (b||c)));
 
-        /* zad.1 proszę dodać kilka złożonych operacji uzywając kilku operatorów arytmetycznych i logicznych */
+
+
 
     }
 }
