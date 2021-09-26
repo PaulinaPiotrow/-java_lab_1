47db39a7eba340a4534f192ef26ebd71d1dcd033
kpaul <kpaulina1995@gmail.com>
2021-09-26 08:45:05 +0200

    Zadanie 1.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,15 @@
 public class Main {
 
-    public static void main(String[] args) {
-//        ćw.1 napisać po kilka przykładów wypisywania tekstu w konsoli,
-//        Użyć każdej z opcji
+    public static void main(String[] args)
+    {
+        System.out.println("Witaj świecie i dzień dobry!");
+        System.out.println("Cieszę się że tu jestem");
+        System.out.println("Super" + " " + "się " + "uczyć w niedziele!");
 
+        System.out.printf("To są %s %d zajęcia.\n", "moje", 2);
 
+        System.out.print("Kocham moje ");
+        System.out.print("dwie najpiękniejsze ");
+        System.out.print("psinki.");
     }
 }
