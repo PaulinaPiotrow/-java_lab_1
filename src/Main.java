8a8f4992484af87e5c6a0171669d4b8e254d7f2a
kpaul <kpaulina1995@gmail.com>
2021-10-01 20:47:02 +0200

    Zadanie 6.

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,15 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
-        /* Tablice - cw_6 */
-
-        /* zad1. stworzyć tablicę typu double 5-cio elementową,
-        przypisać do niej wartości i wyświetlić */
-
+        double[] tab1 = new double[5];
+        tab1[0] = 5.23;
+        tab1[1] = 56;
+        tab1[2] = 124.6;
+        tab1[3] = 6784.9887;
+        tab1[4] = 11.9;
+        for (int i = 0; i < tab1.length; i++)
+        {
+            System.out.println(tab1[i]);
+        }
     }
 }
